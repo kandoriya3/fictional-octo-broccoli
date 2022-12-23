b6410f0dc8f77da014edc349cc1f92f55db4728e
kandoriya3 <86428593+kandoriya3@users.noreply.github.com>
2022-12-23 11:15:49 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
-const express = require('express');
-const multer = require('multer');
+const form = document.getElementById('upload-form');
 
-const app = express();
-const upload = multer({ dest: 'uploads/' });
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
 
-app.post('/upload', upload.single('photo'), (req, res) => {
-  console.log(req.file);
-  res.send('Photo uploaded successfully!');
-});
+  const fileInput = document.querySelector('input[type="file"]');
+  const file = fileInput.files[0];
 
-app.listen(3000, () => {
-  console.log('Server listening on port 3000');
+  const formData = new FormData();
+  formData.append('file', file);
+  formData.append('upload_preset', 'vp7xbx5q');
+
+  fetch('https://api.cloudinary.com/v1_1/dbb45rzoo/image/upload', {
+    method: 'POST',
+    body: formData,
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      // Use the data.secure_url property to display the uploaded photo on your website
+    });
 });
